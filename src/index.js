7c6f7c5b18c78cfe787e3f2fc02dae634ceb667f
Amoorer75 <Amoorer127@gmail.com>
2022-02-27 19:22:23 -0500

    first init

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reducer from "./store/reducer";
+
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+
+const logAction = store => {
+  return next => {
+    return action => {
+      const result = next(action);
+      console.log(
+        `caught in the middleware ${JSON.stringify(store.getState())}`
+      );
+      return result;
+    };
+  };
+};
+
+const store = createStore(reducer, applyMiddleware(logAction));
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById("root")
 );
